670e32b530f0036b9c326cb09ed47332b59c923e
MrMras <artemashitkov02@gmail.com>
2024-03-07 15:14:56 -0600

    Simplified

---
+++ model_structure_3d_2.py
@@ -0,0 +1,64 @@
+#Import the libraries
+import torch.nn as nn
+
+from config import NUM_PICS
+from torch import device
+from torch import cuda
+
+# Define the U-Net architecture
+class UNet3D(nn.Module):
+    def __init__(self):
+        super(UNet3D, self).__init__()
+        self.device = device("cuda:0" if cuda.is_available() else "cpu")
+
+        # Contraction Path (Encoder)
+        self.encoder = nn.Sequential(
+            nn.Conv3d(1, 16, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm3d(16),
+            nn.Dropout3d(0.1),
+            nn.Conv3d(16, 16, 3, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(),
+            nn.MaxPool3d(2),
+
+            nn.Conv3d(16, 32, 3, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU(),
+            nn.Dropout3d(0.3),
+            nn.Conv3d(32, 32, 3, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU()
+        )
+
+        # Expansion Path (Decoder)
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose3d(32, 16, 2, stride=2),
+            nn.ReLU(),
+            nn.Conv3d(16, 16, 3, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(),
+            nn.Dropout3d(0.2),
+            nn.Conv3d(16, 16, 3, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(),
+        )
+
+        # Final output layer
+        self.out_conv = nn.Conv3d(16, 1, 1)
+        self.sigmoid = nn.Sigmoid()
+
+        self.to(self.device)
+
+    def forward(self, x):
+        # Encoder
+        enc_features = self.encoder(x)
+        
+        # Decoder
+        dec_features = self.decoder(enc_features)
+        
+        # Final output
+        out = self.out_conv(dec_features)
+        out = self.sigmoid(out)
+        
+        return out
